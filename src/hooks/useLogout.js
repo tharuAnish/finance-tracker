c582314fc73d66ba818823022c2b449b79f6d88b
tharuAnish <namoanishtharu@gmail.com>
2023-04-29 10:41:04 +0530

    14.18.0 firebase auth state change, MAJOR-taken from shaun because of some error

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from "react"
-import { projectAuth } from "../firebase/config"
-import { useAuthContext } from "./useAuthContext"
+import { useEffect, useState } from 'react'
+import { projectAuth } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const { dispatch } = useAuthContext()
-
+  
   const logout = async () => {
     setError(null)
     setIsPending(true)
@@ -15,16 +15,17 @@ export const useLogout = () => {
     try {
       // sign the user out
       await projectAuth.signOut()
-
+      
       // dispatch logout action
-      dispatch({ type: "LOGOUT" })
+      dispatch({ type: 'LOGOUT' })
 
       // update state
       if (!isCancelled) {
         setIsPending(false)
         setError(null)
-      }
-    } catch (err) {
+      } 
+    } 
+    catch(err) {
       if (!isCancelled) {
         setError(err.message)
         setIsPending(false)
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react"
-import { projectAuth } from "../firebase/config"
-import { useAuthContext } from "./useAuthContext"
+import { useState, useEffect } from 'react'
+import { projectAuth } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
   const [isCancelled, setIsCancelled] = useState(false)
@@ -11,29 +11,27 @@ export const useSignup = () => {
   const signup = async (email, password, displayName) => {
     setError(null)
     setIsPending(true)
-
+  
     try {
       // signup
-      const res = await projectAuth.createUserWithEmailAndPassword(
-        email,
-        password
-      )
+      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
 
       if (!res) {
-        throw new Error("Could not complete signup")
+        throw new Error('Could not complete signup')
       }
 
       // add display name to user
       await res.user.updateProfile({ displayName })
 
       // dispatch login action
-      dispatch({ type: "LOGIN", payload: res.user })
+      dispatch({ type: 'LOGIN', payload: res.user })
 
       if (!isCancelled) {
         setIsPending(false)
         setError(null)
       }
-    } catch (err) {
+    } 
+    catch(err) {
       if (!isCancelled) {
         setError(err.message)
         setIsPending(false)
