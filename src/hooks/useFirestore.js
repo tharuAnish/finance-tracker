263804a5759b99485d7019f87c7317e0fb09308c
tharuAnish <namoanishtharu@gmail.com>
2023-04-30 09:53:32 +0530

    firestore timestamp

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useReducer, useEffect, useState } from "react"
-import { projectFirestore } from "../firebase/config"
+import { projectFirestore, timestamp } from "../firebase/config"
 
 let initialState = {
   document: null,
@@ -57,7 +57,8 @@ export const useFirestore = (collection) => {
   const addDocument = async (doc) => {
     dispatch({ type: "IS_PENDING" })
     try {
-      const addedDocument = await ref.add(doc)
+      const createdAt = timestamp.formDate(new Date())
+      const addedDocument = await ref.add(...doc, createdAt)
       dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payLoad: addedDocument })
     } catch (err) {
       dispatchIfNotCancelled({ type: "ERROR", payload: err.message })
